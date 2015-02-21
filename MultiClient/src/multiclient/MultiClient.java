39b9390fbafade0e830f6a83ba2fb467a3ce6e7a
Mem4o <mitev.e93@gmail.com>
2015-02-21 08:38:14 +0200

    Add Server and Client

---
+++ MultiClient/src/multiclient/MultiClient.java
@@ -0,0 +1,126 @@
+package multiclient;
+
+import java.io.*;
+import static java.lang.System.exit;
+import java.net.*;
+
+public class MultiClient implements Runnable{
+    
+    private static Socket clientSocket = null;
+    private static PrintStream outputStream = null;
+    private static DataInputStream inputStream = null;
+    private static BufferedReader inputLine = null;
+    private static boolean closed = false;
+    int portNumber = 2222;
+    String host = "localhost";
+    
+    public final static int SOCKET_PORT = 13267;      
+    public final static String SERVER = "127.0.0.1";  
+    public final static String FILE_TO_RECEIVED = "C:\\Users\\Emil\\Desktop\\copy.jpg";  
+
+    public final static int FILE_SIZE = 6022386; 
+    
+    public void init(){
+        try {
+            clientSocket = new Socket(host, portNumber);
+            inputLine = new BufferedReader(new InputStreamReader(System.in));
+            outputStream = new PrintStream(clientSocket.getOutputStream());
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+        } 
+        catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + host);
+        } 
+        catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to the host " + host);
+        }
+    }
+    
+    public void start(){
+        if (clientSocket != null && outputStream != null && inputStream != null) {
+            try {
+                new Thread(new MultiClient()).start();
+                while (!closed) {
+                    outputStream.println(inputLine.readLine().trim());
+                }
+                outputStream.close();
+                inputStream.close();
+                clientSocket.close();
+            } 
+            catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        MultiClient client = new MultiClient();
+        client.init();
+        client.start();
+              
+    }
+    
+    @Override
+    public void run() {
+        String responseLine;
+        try {
+            System.out.println("The client started. Type command(\"list\"/\"send\"/\"..\"/\"quit\")");
+            while (true) {
+                responseLine = inputStream.readLine();
+                if (responseLine.contains("quit")) {
+                    exit(1);
+                }
+                else if(responseLine.contains("list")) {
+                    responseLine = inputStream.readLine();
+                    int number = Integer.parseInt(responseLine);
+                    for (int i = 0; i < number; i++) {
+                        responseLine = inputStream.readLine();
+                        System.out.println(responseLine);
+                    }
+                }
+                else if (responseLine.contains("send")) {
+                     ReceivedFile();
+                }
+                else{
+                    System.out.println(responseLine);
+                }
+            }
+        } 
+        catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+    }
+    
+    public static void ReceivedFile () throws IOException {
+      int bytesRead;
+      int current = 0;
+      FileOutputStream fos = null;
+      BufferedOutputStream bos = null;
+      Socket sock = null;
+      try {
+        sock = new Socket(SERVER, SOCKET_PORT);
+        System.out.println("Connecting...");
+
+        // receive file
+        byte [] mybytearray  = new byte [FILE_SIZE];
+        InputStream is = sock.getInputStream();
+        fos = new FileOutputStream(FILE_TO_RECEIVED);
+        bos = new BufferedOutputStream(fos);
+        bytesRead = is.read(mybytearray,0,mybytearray.length);
+        current = bytesRead;
+
+        do {
+           bytesRead = is.read(mybytearray, current, (mybytearray.length-current));
+           if(bytesRead >= 0) current += bytesRead;
+        } while(bytesRead > -1);
+
+        bos.write(mybytearray, 0 , current);
+        bos.flush();
+        System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
+      }
+      finally {
+        if (fos != null) fos.close();
+        if (bos != null) bos.close();
+        if (sock != null) sock.close();
+      }
+    }
+}
---
+++ MultiServer/src/multiserver/MultiServer.java
@@ -0,0 +1,182 @@
+package multiserver;
+
+import java.io.*;
+import static java.lang.System.exit;
+import java.net.*;
+
+public class MultiServer {
+    
+    private static ServerSocket serverSocket = null;
+    private static Socket clientSocket = null;
+    private static final int maxClientsCount = 10;
+    private static final clientThread[] threads = new clientThread[maxClientsCount];
+    int portNumber = 2222;
+    
+    public void init (){
+        try {
+            serverSocket = new ServerSocket(portNumber);
+            System.out.println("Server is start on port: " + portNumber);
+        }    
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+    public void start (){
+         while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                int i = 0;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        (threads[i] = new clientThread(clientSocket, threads)).start();
+                        break;
+                    }
+                }
+                if (i == maxClientsCount) {
+                    PrintStream output = new PrintStream(clientSocket.getOutputStream());
+                    output.println("Server too busy. Try later.");
+                    output.close();
+                    clientSocket.close();
+                }
+            } 
+            catch (IOException e) {
+                System.out.println(e);
+            }
+        }   
+    }
+   
+    
+    public static void main(String[] args) {
+       MultiServer server = new MultiServer();
+       server.init();
+       server.start();
+       
+        
+    }
+}
+
+class clientThread extends Thread {
+    
+    private DataInputStream inputStream = null;
+    private PrintStream outputStream = null;
+    private Socket clientSocket = null;
+    private final clientThread[] threads;
+    private final int maxClientsCount;
+    public final static int SOCKET_PORT = 13267;
+    public static String FILE_TO_SEND = "D:";
+
+    public clientThread(Socket clientSocket, clientThread[] threads) {
+        this.clientSocket = clientSocket;
+        this.threads = threads;
+        maxClientsCount = threads.length;
+    }
+
+    @Override
+    public void run() {
+        int maxClientsCount = this.maxClientsCount;
+        clientThread[] threads = this.threads;
+        String line;
+        try {
+            inputStream = new DataInputStream(clientSocket.getInputStream());
+            outputStream = new PrintStream(clientSocket.getOutputStream());
+            File[] listDir = null;
+            do {
+                line = inputStream.readLine();
+                System.out.println(line);
+                if (line.contains("quit")) {
+                    outputStream.println(line);
+                    exit(1);
+                }
+                else if (line.contains("list")) {
+                    listDir = ListDirectory(FILE_TO_SEND);
+                    outputStream.println(line);  
+                    outputStream.println(listDir.length);  
+                    for (File list1 : listDir) {
+                        if (list1.isFile()) {
+                            outputStream.println("File " + list1.getName());
+                        } else if (list1.isDirectory()) {
+                            outputStream.println("Directory " + list1.getName());
+                        }
+                    }       
+                }
+                else if (line.contains("send")) {
+                    outputStream.println(line);
+                    SendFile();
+                }
+                else if (line.contains("..")) {
+                    int index = FILE_TO_SEND.lastIndexOf('\\');
+                    FILE_TO_SEND = FILE_TO_SEND.substring(0, index);
+                    outputStream.println(line);
+                    listDir = null;
+                }
+                else{   
+                    boolean isTrue = false;
+                    if (listDir != null) {
+                        for (File list1 : listDir) {
+                            String str = list1.toString();
+                            if (str.contains(line)) {
+                                isTrue = true;
+                                break;
+                            }
+                        }  
+                    }
+                    if (isTrue) {
+                        FILE_TO_SEND = FILE_TO_SEND + "\\" + line;
+                        listDir = null;
+                        outputStream.println(line);
+                    }
+                    else{
+                        outputStream.println("Wrong command");
+                    }
+                }
+            } while (true);
+        } 
+        catch (IOException e) {
+            System.out.println(e);
+        }
+        }
+    public static File[] ListDirectory (String directory) {
+        File folder = new File(directory);
+        File[] listOfFiles = folder.listFiles();
+        return listOfFiles;
+    }
+    
+    public static void SendFile () throws IOException {
+      FileInputStream fis = null;
+      BufferedInputStream bis = null;
+      OutputStream os = null;
+      ServerSocket servsock = null;
+      Socket sock = null;
+      try {
+        servsock = new ServerSocket(SOCKET_PORT);
+        boolean isTrue = true;
+        while (isTrue) {
+          System.out.println("Waiting...");
+          try {
+            sock = servsock.accept();
+            System.out.println("Accepted connection : " + sock);
+            // send file
+            File myFile = new File (FILE_TO_SEND);
+            byte [] mybytearray  = new byte [(int)myFile.length()];
+            fis = new FileInputStream(myFile);
+            bis = new BufferedInputStream(fis);
+            bis.read(mybytearray,0,mybytearray.length);
+            os = sock.getOutputStream();
+            System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+            os.write(mybytearray,0,mybytearray.length);
+            os.flush();
+            System.out.println("Done.");
+            isTrue = false;
+          }
+          finally {
+            if (bis != null) bis.close();
+            if (os != null) os.close();
+            if (sock!=null) sock.close();
+          }
+        }
+      }
+      finally {
+        if (servsock != null) servsock.close();
+      }
+    }
+    }
